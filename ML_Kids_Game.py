2562a18a0d9e6b02ef1cab75e450402b7bc753c6
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-08-31 22:47:21 +0600

    add changes

--- ML_Kids_Game.py
+++ ML_Kids_Game.py
@@ -5,7 +5,13 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
+detector = FaceMeshDetector(maxFaces=1)
+
 while True:
     success, img = cap.read()
+    img, faces = detector.findFaceMesh(img)
+    if faces:
+        print(faces[0])
+        
     cv2.imshow("Image", img)
     cv2.waitKey(1)
